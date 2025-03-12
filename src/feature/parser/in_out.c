874d005c7251ad83be6fc667e91629eec3872cee
Julia Helbig <jhelbig@c3a1c1.42berlin.de>
2025-03-12 14:18:44 +0100

    feat: add structs, start parser/in_out file structure

---
+++ src/feature/parser/in_out.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   in_out.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jhelbig <jhelbig@student.42berlin.de>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/12 13:21:47 by jhelbig           #+#    #+#             */
+/*   Updated: 2025/03/12 13:33:16 by jhelbig          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+//getting the split input
+//looking for > and < to set infile and output
+char	**in_out(char **args)
+{
+	
+}
+
+// finding > in splits and set infile
+//options: >infile > infile
+char **infile(char **args)
+{
+	
+}
