100d347d6a1c8d95a873853d4d8f74745f48179d
Julia Helbig <jhelbig@c3a1c1.42berlin.de>
2025-03-12 15:20:45 +0100

    feat: in_out parser

--- src/parser/in_out.c
+++ src/parser/in_out.c
@@ -6,7 +6,7 @@
 /*   By: jhelbig <jhelbig@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/12 13:21:47 by jhelbig           #+#    #+#             */
-/*   Updated: 2025/03/12 13:33:16 by jhelbig          ###   ########.fr       */
+/*   Updated: 2025/03/12 15:17:19 by jhelbig          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,43 @@
 //looking for > and < to set infile and output
 char	**in_out(char **args)
 {
-	
+	t_cmd_type	cmd;
+
+	infile(args, &cmd);
+	outfile(args, &cmd);
+	trim_args(args);
 }
 
 // finding > in splits and set infile
-//options: >infile > infile
-char **infile(char **args)
+//options: 1) >infile 2) > infile
+int **infile(char **args, t_cmd_type *cmd)
 {
-	
+	int		i;
+	char	*file;
+
+	i = 0;
+	while (args[i])
+	{
+		//option 1)
+		if ((args[i][0] == '<') && (ft_strlen(args[i]) > 1))
+		{
+			file = ft_strtrim(args[i], <);
+			cmd->infile = open(file, O_RDONLY);
+			// TO DO: ERROR-HANDLING
+			free(file);
+			args[i] = NULL;
+		}
+		//option 2)
+		else if (args[i][0] == '<' && ft_strlen(args[i]) == 1)
+		{
+			// TO DO: error if < is last char* --> no infile given
+			file = args[i + 1];
+			cmd->infile = open(file, O_RDONLY);
+			args[i] = NULL;
+			args[i + 1] = NULL;
+		}
+		i++;
+		
+	}
+	return (0);	
 }
