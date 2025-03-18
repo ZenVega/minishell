0279436a94bf60999d55d326f7ef205511250bd6
Julia Helbig <jhelbig@c3a1c1.42berlin.de>
2025-03-18 13:43:24 +0100

    feat: add function to add address malloc from another function to the malloc_list

--- src/malloc_list/malloc_list.c
+++ src/malloc_list/malloc_list.c
@@ -6,13 +6,14 @@
 /*   By: jhelbig <jhelbig@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/18 11:57:04 by jhelbig           #+#    #+#             */
-/*   Updated: 2025/03/18 13:29:04 by jhelbig          ###   ########.fr       */
+/*   Updated: 2025/03/18 13:42:33 by jhelbig          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-void	*add_malloc_list(t_list **malloc_list, size_t size)
+//to be used instead of malloc
+void	*malloc_and_add_list(t_list **malloc_list, size_t size)
 {
 	void	*addr;
 	t_list	*new_node;
@@ -22,6 +23,16 @@ void	*add_malloc_list(t_list **malloc_list, size_t size)
 	ft_lstadd_back(malloc_list, new_node);
 	return (addr);
 }
+
+//if another function does malloc
+void	add_to_malloc_list(t_list **malloc_list, void *addr)
+{
+	t_list	*new_node;
+	
+	new_node = ft_lstnew(addr);
+	ft_lstadd_back(malloc_list, new_node);
+}
+
 
 // to free the list call:
 // ft_lstclear(**malloc_list, free)
