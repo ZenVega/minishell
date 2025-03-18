d0ecb0a64f66d02f2e2c3a76efceea64f6793212
Urs Schmidt <ufogreeks@gmail.com>
2025-03-18 17:04:06 +0100

    feat: check for invalid arguments

--- src/minishell.c
+++ src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: uschmidt <uschmidt@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/11 13:27:39 by uschmidt          #+#    #+#             */
-/*   Updated: 2025/03/18 16:54:22 by uschmidt         ###   ########.fr       */
+/*   Updated: 2025/03/18 17:03:09 by uschmidt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,11 @@ int	main(int argc, char **argv, char *envp[])
 {
 	t_app	*app;
 
+	if (argc != 1)
+		return (1);
+	//errorhandling
 	ft_printf("Hello %s\n", argv[0]);
 	app = init_shell(envp);
 	free_malloc_list(app);
-	return (argc);
+	return (0);
 }
