fff713add8aac2e19940f9dc627789e4ccdf9d7c
Urs Schmidt <ufogreeks@gmail.com>
2025-03-18 16:59:45 +0100

    chores: remove testcode

--- src/init/init.c
+++ src/init/init.c
@@ -6,7 +6,7 @@
 /*   By: uschmidt <uschmidt@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/18 14:03:56 by uschmidt          #+#    #+#             */
-/*   Updated: 2025/03/18 16:42:53 by uschmidt         ###   ########.fr       */
+/*   Updated: 2025/03/18 16:59:33 by uschmidt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,12 +23,5 @@ t_app	*init_shell(char *envp[])
 	//error handling here
 	app->malloc_list = NULL;
 	app->envp = envp;
-	test = malloc_and_add_list(&app->malloc_list, (sizeof(char) * 5));
-	test[0] = 'H';
-	while (0 && *envp)
-	{
-		ft_printf("%s\n", *envp);
-		envp++;
-	}
 	return (app);
 }
