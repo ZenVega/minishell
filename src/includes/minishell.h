197a04fc153b2f7f4dcf16aebbd1c6ff24e79e9a
Julia Helbig <jhelbig@c3a1c1.42berlin.de>
2025-03-18 17:11:12 +0100

    fix: minishell.h

--- src/includes/minishell.h
+++ src/includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: uschmidt <uschmidt@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/11 16:28:24 by uschmidt          #+#    #+#             */
-/*   Updated: 2025/03/18 15:37:08 by uschmidt         ###   ########.fr       */
+/*   Updated: 2025/03/18 17:10:11 by jhelbig          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 # include "../libft/libft.h"
 # include <readline/readline.h>
 # include <readline/history.h>
+# include <fcntl.h>
 
 typedef enum e_cmd_type {
 	LOGICAL,
