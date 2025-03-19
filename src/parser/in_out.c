8fa94d73adbfc10d4205c64c6557c90e29fef045
Julia Helbig <jhelbig@c3a1c1.42berlin.de>
2025-03-19 12:00:37 +0100

    chores: more on infile redir

--- src/parser/in_out.c
+++ src/parser/in_out.c
@@ -6,7 +6,7 @@
 /*   By: jhelbig <jhelbig@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/12 13:21:47 by jhelbig           #+#    #+#             */
-/*   Updated: 2025/03/19 09:24:05 by jhelbig          ###   ########.fr       */
+/*   Updated: 2025/03/19 11:25:41 by jhelbig          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,25 +32,64 @@ void	set_infile(char **args, t_cmd_info *cmd, t_list **malloc_list)
 
 	i = 0;
 	while (args[i])
-	{
-		if ((args[i][0] == '<') && (ft_strlen(args[i]) > 1) && args[i][1] != '<')
+	{	
+		if (args[i][0] == '<')
 		{
-			file_name = ft_strtrim(args[i], (const char *) "<");
-			add_to_malloc_list(malloc_list, (void *) file_name);
-			cmd->infile = open(file_name, O_RDONLY);
-			if (cmd->infile < 0)
-				no_infile(file_name, malloc_list);
+			// case < infile
+			if (ft_strlen(args[i]) == 1)
+			{
+				if (args[i + 1])
+					file_name = args[i + 1];
+				else
+					parse_error_near_nl(malloc_list);
+				cmd->infile = open(file_name, O_RDONLY);
+				if (cmd->infile < 0)
+					no_infile(file_name, malloc_list);
+			}
+			if (ft_strlen(args[i]) > 1)
+			{
+				// case <infile
+				if (args[i][1] != '<')
+				{
+					file_name = ft_strtrim(args[i], (const char *) "<");
+					add_to_malloc_list(malloc_list, (void *) file_name);
+					cmd->infile = open(file_name, O_RDONLY);
+					if (cmd->infile < 0)
+						no_infile(file_name, malloc_list);
+				}
+				//here_doc <<
+				//if (args[i][1] == '<')
+					//here_doc(args, i, cmd, malloc_list);	
+			}	
 		}
-		else if (args[i][0] == '<' && ft_strlen(args[i]) == 1)
-		{
-			if (args[i + 1])
-				file_name = args[i + 1];
-			else
-				parse_error_near_nl(malloc_list);
-			cmd->infile = open(file_name, O_RDONLY);
-			if (cmd->infile < 0)
-				no_infile(file_name, malloc_list);
-		}
-		i++;
+	i++;
 	}	
+}
+
+//problem: here_doc muss vermutlich readline verwenden, statt get_next_line
+// options: ">>END" and ">>" "END"
+void	here_doc(char **args, int i, t_cmd_info *cmd, t_list **malloc_list)
+{
+	int		fd;
+	char	*limiter;
+	char	*next_line;
+
+	if (args[i + 1])
+		limiter = args[i + 1];
+	// what happens on single << without delimiter? - parser error near new_line
+	fd = open("here_doc", O_RDWR | O_CREAT | O_TRUNC | O_APPEND, 0777);
+	while (1)
+	{
+		next_line = get_next_line(STDIN_FILENO);
+		if (ft_strncmp(limiter, next_line, ft_strlen(limiter)) == 0)
+		{
+			free(next_line);
+			close(fd);
+			cmd->infile = fd; 
+			return ;
+		}
+		write(fd, next_line, ft_strlen(next_line));
+		free(next_line);
+	}
+	return ;
 }
