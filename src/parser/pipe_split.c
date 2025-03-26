2aa8092cfd634895471cf47629a4c379fd728cda
Urs Schmidt <ufogreeks@gmail.com>
2025-03-26 14:20:56 +0100

    hotfix: double free in pipe_split

--- src/parser/pipe_split.c
+++ src/parser/pipe_split.c
@@ -18,7 +18,7 @@
 /*
 gives back a left side and a right side of the pipe for further handling
 */
-char **pipe_split(char *line, t_list **malloc_list)
+char	**pipe_split(char *line, t_list **malloc_list)
 {
 	char	*part;
 	char	**parts;
@@ -28,25 +28,18 @@ char **pipe_split(char *line, t_list **malloc_list)
 	if (part)
 	{
 		parts = malloc_and_add_list(malloc_list, sizeof(char *) * 3);
- 		parts[2] = NULL;
-		
+		parts[2] = NULL;
 		//left part
 		len = part - line;
 		parts[0] = malloc_and_add_list(malloc_list, len + 1);
 		if (parts[0])
 			ft_strlcpy(parts[0], line, len);
 		parts[0][len] = '\0';
-
 		//right part
 		parts[1] = ft_strdup(part + 1);
-		if(!parts[0] || !parts[1])
-		{
-			free(parts[0]);
-			free(parts[1]);
-			return (NULL);
-		}		
-		add_to_malloc_list(malloc_list, parts[0]);
 		add_to_malloc_list(malloc_list, parts[1]);
+		if (!parts[0] || !parts[1])
+			return (NULL);
 	}
 	else
 		return (NULL);
