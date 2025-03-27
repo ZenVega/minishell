67ba46d98c2900d74629da0983edeabace73c708
Urs Schmidt <ufogreeks@gmail.com>
2025-03-27 11:29:04 +0100

    feat: add basic pipe setup

--- src/exe/exe.h
+++ src/exe/exe.h
@@ -6,7 +6,7 @@
 /*   By: uschmidt <uschmidt@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/12 14:58:25 by uschmidt          #+#    #+#             */
-/*   Updated: 2025/03/19 11:06:28 by uschmidt         ###   ########.fr       */
+/*   Updated: 2025/03/27 10:51:47 by uschmidt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef EXE_H
@@ -20,6 +20,7 @@
 # include <stdlib.h>
 # include <sys/wait.h>
 # include <sys/types.h>
+# include "../parser/parser.h"
 
 typedef struct s_exe {
 	char	**args;
@@ -31,6 +32,8 @@ typedef struct s_exe {
 int		exe(t_app *app, t_cmd_info *cmd);
 //exe_bin.c
 int		exe_bin(t_app *app, t_cmd_info *cmd);
+//exe_pip
+int		open_pipe(t_app *app, t_cmd_info *cmd);
 //exe_utils.c
 int		is_valid(t_cmd_info *cmd);
 void	reroute_io(int input, int output);
---
+++ src/exe/exe_pipe.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exe_pipe.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: uschmidt <uschmidt@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/27 10:02:31 by uschmidt          #+#    #+#             */
+/*   Updated: 2025/03/27 11:08:02 by uschmidt         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "exe.h"
+
+t_parser_info	init_parser_info(int infile, int outfile, char *line)
+{
+	t_parser_info	p_info;
+
+	p_info.infile = infile;
+	p_info.outfile = outfile;
+	p_info.line = line;
+	return (p_info);
+}
+
+int	open_pipe(t_app *app, t_cmd_info *cmd)
+{
+	pid_t			pids[2];
+	int				fd[2];
+	t_parser_info	p_info;
+
+	pids[0] = fork();
+	if (pids[0] == -1)
+	{
+		perror("fork");
+		return (1);
+	}
+	// TODO: error check
+	if (pids[0] == 0)
+	{
+		if (pipe(fd) < 0)
+			return (errno = SIGPIPE, errno);
+		pids[1] = fork();
+		if (pids[1] == 0) //Child process - write to parent
+		{
+			p_info = init_parser_info(cmd->infile, fd[1], cmd->args[0]);
+			exe(app, parser(p_info, &app->malloc_list));
+		}
+		else //Parent process - read from child
+		{
+			p_info = init_parser_info(fd[0], cmd->outfile, cmd->args[1]);
+			exe(app, parser(p_info, &app->malloc_list));
+			// Check for input file
+		}
+	}
+	else
+		wait(NULL);
+	return (0);
+}
