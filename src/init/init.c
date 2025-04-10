93d66fa24d7637c791c0312f068586ebdf1635a2
Julia Helbig <jhelbig@c3a2c9.42berlin.de>
2025-04-10 15:20:32 +0200

    fix: envp

--- src/init/init.c
+++ src/init/init.c
@@ -6,7 +6,7 @@
 /*   By: uschmidt <uschmidt@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/18 14:03:56 by uschmidt          #+#    #+#             */
-/*   Updated: 2025/04/10 15:07:45 by jhelbig          ###   ########.fr       */
+/*   Updated: 2025/04/10 15:18:58 by jhelbig          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,16 @@ char	**copy_envp(char **envp, t_list **malloc_list)
 	int		i;
 	char	**copy;
 
+	malloc_list = NULL;
 	size = 0;
 	while (envp[size])
 		size++;
-	copy = malloc_and_add_list(malloc_list, sizeof(char *) * size + 1);
+	copy = (char **)malloc(sizeof(char *) * (size + 1));
 	i = 0;
 	while (envp[i])
 	{
 		copy[i] = ft_strdup(envp[i]);
-		add_to_malloc_list(malloc_list, copy[i]);
+		//add_to_malloc_list(malloc_list, copy[i]);
 		i++;
 	}
 	copy[i] = NULL;
@@ -43,14 +44,14 @@ t_app	*init_shell(char *envp[])
 		return (NULL);
 	app->malloc_list = NULL;
 	app->envp = copy_envp(envp, &app->malloc_list);
-	/*
+/*	
 	int i = 0;
 	while (app->envp[i])
 	{
 		ft_printf("%s\n", app->envp[i]);
 		i++;
 	}
-	*/
+*/	
 	app->prompt = NULL;
 	return (app);
 }
