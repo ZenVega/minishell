41342a0fffd4fbe5ebcce14a43fa0a9cd7500527
Urs Schmidt <ufogreeks@gmail.com>
2025-04-11 10:22:39 +0200

    feat: add create_mask

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -11,7 +11,43 @@
 /* ************************************************************************** */
 
 #include "parser.h"
-#include "../includes/minishell.h"
+
+int	create_mask(t_parser_info p_info, t_list **malloc_list, t_cmd_info *cmd)
+{
+	int		i;
+	char	*tmp;
+	int		len;
+
+	i = 0;
+	len = ft_strlen(p_info.line);
+	p_info.mask = (int *)malloc_and_add_list(malloc_list, sizeof(int) * len);
+	if (!p_info.mask)
+		return (set_err(cmd, ERR_MALLOC, NULL));
+	while (p_info.line[i])
+	{
+		while (p_info.line[i] != '"' && p_info.line[i] != '\'')
+			p_info.mask[i++] = 0;
+		if (p_info.line[i] == '"')
+		{
+			tmp = ft_strchr((p_info.line + i), '"');
+			if (tmp == NULL)
+				p_info.mask[i++] = 0;
+			else
+				while (p_info.line + i <= tmp)
+					p_info.mask[i++] = 2;
+		}
+		if (p_info.line[i] == '\'')
+		{
+			tmp = ft_strchr((p_info.line + i), '\'');
+			if (tmp == NULL)
+				p_info.mask[i++] = 0;
+			else
+				while (p_info.line + i <= tmp)
+					p_info.mask[i++] = 1;
+		}
+	}
+	return (0);
+}
 
 t_cmd_info	*parser(t_parser_info p_info, t_list **malloc_list)
 {
@@ -23,8 +59,9 @@ t_cmd_info	*parser(t_parser_info p_info, t_list **malloc_list)
 	if (!cmd)
 		return (NULL);
 	//find pipe, split on first pipe
-	parts = malloc_and_add_list (malloc_list, sizeof(char *) * 3);
-	if (!parts)
+	parts = (char **)malloc_and_add_list (malloc_list, sizeof(char *) * 3);
+	err = create_mask(p_info, malloc_list, cmd);
+	if (err == -1 || !parts)
 		return (NULL);
 	err = pipe_split(parts, p_info.line, malloc_list);
 	//pipe failed
