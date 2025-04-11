cdd44ba4a64fce4252e11e5384c5ff2c6330df31
Urs Schmidt <ufogreeks@gmail.com>
2025-04-11 11:54:40 +0200

    chores: refactoring

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -12,10 +12,29 @@
 
 #include "parser.h"
 
+int	mask_substr(char *line, int pos, int *mask, char c)
+{
+	char	*tmp;
+	int		mask_id;
+
+	mask_id = 1;
+	if (c == '"')
+		mask_id = 2;
+	tmp = ft_strchr(((&line[pos]) + 1), c);
+	if (tmp == NULL)
+	{
+		mask[pos] = 0;
+		return (pos + 1);
+	}
+	else
+		while (line + pos <= tmp)
+			mask[pos++] = mask_id;
+	return (pos);
+}
+
 int	create_mask(t_parser_info p_info, t_list **malloc_list, t_cmd_info *cmd)
 {
 	int		i;
-	char	*tmp;
 	int		len;
 
 	i = 0;
@@ -25,29 +44,16 @@ int	create_mask(t_parser_info p_info, t_list **malloc_list, t_cmd_info *cmd)
 		return (set_err(cmd, ERR_MALLOC, NULL));
 	while (p_info.line[i])
 	{
-		while (p_info.line[i] != '"' && p_info.line[i] != '\'')
+		while (p_info.line[i]
+			&& p_info.line[i] != '"' && p_info.line[i] != '\'')
 			p_info.mask[i++] = 0;
-		if (p_info.line[i] == '"')
-		{
-			tmp = ft_strchr((p_info.line + i), '"');
-			if (tmp == NULL)
-				p_info.mask[i++] = 0;
-			else
-				while (p_info.line + i <= tmp)
-					p_info.mask[i++] = 2;
-		}
-		if (p_info.line[i] == '\'')
-		{
-			tmp = ft_strchr((p_info.line + i), '\'');
-			if (tmp == NULL)
-				p_info.mask[i++] = 0;
-			else
-				while (p_info.line + i <= tmp)
-					p_info.mask[i++] = 1;
-		}
+		if (p_info.line[i] == '"' || p_info.line[i] == '\'')
+			i = mask_substr(p_info.line, i, p_info.mask, p_info.line[i]);
 	}
-	for (int i = 0; i <= len; i++)
-		ft_printf("%d-", p_info.mask[i]);
+	//ft_printf("MASK: ", p_info.mask[i]);
+	//for (int i = 0; i < len; i++)
+	//	ft_printf("%d", p_info.mask[i]);
+	//ft_printf("\n", p_info.mask[i]);
 	return (0);
 }
 
