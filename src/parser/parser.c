0df67bab0aa198d1c893bf51991fe9424a1dafc6
zen_vega <ufogreeks@gmail.com>
2025-04-14 16:38:48 +0200

    fix: segfault

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -32,28 +32,28 @@ int	mask_substr(char *line, int pos, int *mask, char c)
 	return (pos);
 }
 
-int	create_mask(t_parser_info p_info, t_list **malloc_list, t_cmd_info *cmd)
+int	create_mask(t_parser_info *p_info, t_list **malloc_list, t_cmd_info *cmd)
 {
 	int		i;
 	int		len;
 
 	i = 0;
-	len = ft_strlen(p_info.line);
-	p_info.mask = (int *)malloc_and_add_list(malloc_list, sizeof(int) * len);
-	if (!p_info.mask)
+	len = ft_strlen(p_info->line);
+	p_info->mask = (int *)malloc_and_add_list(malloc_list, sizeof(int) * len);
+	if (!p_info->mask)
 		return (set_err(cmd, ERR_MALLOC, NULL));
-	while (p_info.line[i])
+	while (p_info->line[i])
 	{
-		while (p_info.line[i]
-			&& p_info.line[i] != '"' && p_info.line[i] != '\'')
-			p_info.mask[i++] = 0;
-		if (p_info.line[i] == '"' || p_info.line[i] == '\'')
-			i = mask_substr(p_info.line, i, p_info.mask, p_info.line[i]);
+		while (p_info->line[i]
+			&& p_info->line[i] != '"' && p_info->line[i] != '\'')
+			p_info->mask[i++] = 0;
+		if (p_info->line[i] == '"' || p_info->line[i] == '\'')
+			i = mask_substr(p_info->line, i, p_info->mask, p_info->line[i]);
 	}
-	ft_printf("MASK: ", p_info.mask[i]);
-	for (int i = 0; i < len; i++)
-		ft_printf("%d", p_info.mask[i]);
-	ft_printf("\n", p_info.mask[i]);
+	//ft_printf("MASK: ", p_info->mask[i]);
+	//for (int i = 0; i < len; i++)
+	//	ft_printf("%d", p_info->mask[i]);
+	//ft_printf("\n", p_info->mask[i]);
 	return (0);
 }
 
@@ -69,7 +69,7 @@ t_cmd_info	*parser(t_parser_info p_info, t_list **malloc_list)
 	//find pipe, split on first pipe
 	parts = (char **)malloc_and_add_list (malloc_list, sizeof(char *) * 3);
 	free(p_info.mask);
-	err = create_mask(p_info, malloc_list, cmd);
+	err = create_mask(&p_info, malloc_list, cmd);
 	if (err == -1 || !parts)
 		return (NULL);
 	err = pipe_split(parts, &p_info, malloc_list);
