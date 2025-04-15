ff0794326491d70f77331d880fe50da5bb49d554
Julia Helbig <jhelbig@c3a1c7.42berlin.de>
2025-04-15 13:54:31 +0200

    feat: rerouting in env

--- src/buildins/envp/env.c
+++ src/buildins/envp/env.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   env.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jhelbig <jhelbig@student.42berlin.de>      +#+  +:+       +#+        */
+/*   By: jhelbig <jhelbig@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 13:41:06 by jhelbig           #+#    #+#             */
-/*   Updated: 2025/04/15 13:47:25 by jhelbig          ###   ########.fr       */
+/*   Updated: 2025/04/15 13:53:14 by jhelbig          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,15 @@ int	env(t_app *app, t_cmd_info *cmd)
 	int i;
 
 	i = 0;
-	while(app->envp[i])
+	reroute_io(cmd->infile, cmd->outfile);
+	while(cmd && app->envp[i])
 	{
 		ft_printf("%s\n", app->envp[i]);
 		i++;
 	}
+	if (cmd->infile != 0)
+		close(cmd->infile);
+	if (cmd->outfile != 1)
+		close(cmd->outfile);
 	return (0);
 }
--- src/buildins/envp/env.h
+++ src/buildins/envp/env.h
@@ -6,12 +6,13 @@
 /*   By: jhelbig <jhelbig@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/15 13:41:56 by jhelbig           #+#    #+#             */
-/*   Updated: 2025/04/15 13:45:57 by jhelbig          ###   ########.fr       */
+/*   Updated: 2025/04/15 13:51:30 by jhelbig          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ENV_H
 # define ENV_H
+# include "../../exe/exe.h"
 # include "../../includes/types.h"
 # include "../../error/error.h"
 
