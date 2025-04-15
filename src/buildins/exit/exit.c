55a1a41db54b285dc82f3132a8c6c25aec03c172
Julia Helbig <jhelbig@c3a1c7.42berlin.de>
2025-04-15 12:00:50 +0200

    feat: handling different error situations for exit

--- src/buildins/exit/exit.c
+++ src/buildins/exit/exit.c
@@ -1,12 +1,39 @@
 
 #include "exit.h"
+// exit $? = 0
+//exit 4 $? = 4
+
+//exit ab 
+//exit \n bash: exit: ab: numeric argument required
+//$? = 2
+
+//exit 7 5
+//exit
+//bash: exit: too many arguments
+//$? = 1
+
 
 int	ft_exit(t_app *app, t_cmd_info *cmd)
 {
 	int     err;
-    
-    if (cmd->args[1] && ft_isdigit(cmd->args[1][0]))
-        err = cmd->args[1][0];
+
+    //too many arguments
+    if (cmd->args[2])
+    {
+        err = 1;
+        ft_fprintf(1, "exit\n");
+        ft_fprintf(2, "minishell: exit: too many arguments\n");
+    }
+    //non-numeric argument
+    else if (cmd->args[1])
+        if (!ft_isnumber(cmd->args[1]))
+        {
+            err = 2;
+            ft_fprintf(1, "exit\n");
+            ft_fprintf(2, "minishell: exit: %s: numeric argument requiered\n", cmd->args[1]);
+        }
+        else
+            err = ft_atoi(cmd->args[1]);
     else
         err = 0;
     free_malloc_list(app);
