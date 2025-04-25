e06c1540de39526c0c37169b710f97706a88d9a2
Urs Schmidt <ufogreeks@gmail.com>
2025-04-25 14:00:24 +0200

    chores: remove comments

--- src/parser/set_io_files.c
+++ src/parser/set_io_files.c
@@ -6,7 +6,7 @@
 /*   By: jhelbig <jhelbig@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/12 13:21:47 by jhelbig           #+#    #+#             */
-/*   Updated: 2025/04/08 15:24:55 by jhelbig          ###   ########.fr       */
+/*   Updated: 2025/04/25 13:58:27 by uschmidt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,16 +20,13 @@ int	set_io_files(char *line, t_cmd_info *cmd, t_list **malloc_list, int *mask)
 	char	**split;
 	int		err;
 
-	//TODO: replace with safesplit and check for masked spaces
 	split = ft_split_safe(line, ' ', mask);
 	if (!split)
 		return (-1);
 	add_list_to_malloc_list(malloc_list, (void *)split);
-	//multiple infiles are just overwritten
 	err = set_infile(split, cmd);
 	if (err != 0)
 		return (err);
-	//multiple oufile are overwritten by last, but still opened/created/closed on the way
 	err = set_outfile(split, cmd);
 	if (err != 0)
 		return (err);
@@ -47,8 +44,6 @@ void	trim_args(char **args, t_cmd_info *cmd)
 	j = 0;
 	while (args[i])
 	{
-		//case < infile, > outfile, >> outfile
-		// 'delete' 2 args 
 		if (((args[i][0] == '>' || args[i][0] == '<') && ft_strlen(args[i]) == 1) 
 			|| (args[i][0] == '>' && args[i][1] == '>' && ft_strlen(args[i]) == 2))
 			i = i + 2;
