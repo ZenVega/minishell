b8598f396764f9fc05922cd3e4cc06a0e6deb9d8
Julia Helbig <jhelbig@c3a1c7.42berlin.de>
2025-05-05 15:12:06 +0200

    fix: redo outfile

--- src/parser/outfile.c
+++ src/parser/outfile.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   outfile.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jhelbig <jhelbig@student.42berlin.de>      +#+  +:+       +#+        */
+/*   By: jhelbig <jhelbig@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/21 13:20:23 by jhelbig           #+#    #+#             */
-/*   Updated: 2025/04/02 11:11:32 by jhelbig          ###   ########.fr       */
+/*   Updated: 2025/05/05 15:04:51 by jhelbig          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,15 @@ int	set_outfile(char **args, t_cmd_info *cmd)
 {
 	int		i;
 	int		err;
-
+	char	*addr;
+	
 	i = 0;
 	while (args[i])
 	{
-		if (args[i][0] == '>')
+		addr = ft_strchr(args[i], '>');
+		if (addr)
 		{
-			err = found_outfile(args, i, cmd);
+			err = found_outfile(args, i, addr, cmd);
 			if (err != 0)
 				return (err);
 		}
@@ -31,12 +33,12 @@ int	set_outfile(char **args, t_cmd_info *cmd)
 	return (0);
 }
 
-int	found_outfile(char **args, int i, t_cmd_info *cmd)
+int	found_outfile(char **args, int i, char *addr, t_cmd_info *cmd)
 {
 	char	*file_name;
 
 	file_name = NULL;
-	if (ft_strlen(args[i]) == 1)
+	if (ft_strlen(addr) == 1)
 	{
 		if (args[i + 1])
 			file_name = args[i + 1];
@@ -44,14 +46,14 @@ int	found_outfile(char **args, int i, t_cmd_info *cmd)
 			return (set_err(cmd, ERR_SYNTAX, "after >"));
 		return (truncate_outfile(file_name, cmd));
 	}
-	else if (ft_strlen(args[i]) > 1)
+	else if (ft_strlen(addr) > 1)
 	{
 		// case >outfile
-		if (args[i][1] != '>')
-			return (truncate_outfile(args[i] + 1, cmd));
+		if (*(addr + 1) != '>')
+			return (truncate_outfile(addr + 1, cmd));
         //case_append outfile >>
-		else if (args[i][1] == '>')
-			return (append_outfile(args, i, cmd));
+		else if (*(addr + 1) == '>')
+			return (append_outfile(args, i, addr, cmd));
 	}
 	return (0);
 }
@@ -67,14 +69,14 @@ int	truncate_outfile(char *file_name, t_cmd_info *cmd)
 	return (0);
 }
 
-int	append_outfile(char **args, int i, t_cmd_info *cmd)
+int	append_outfile(char **args, int i, char *addr, t_cmd_info *cmd)
 {
 	char	*file_name;
 
 	if (cmd->outfile != STDOUT_FILENO)
 		close(cmd->outfile);
     // case >> outfile
-	if (ft_strlen(args[i]) == 2)
+	if (ft_strlen(addr) == 2)
 	{
 		if (args[i + 1])
 			file_name = args[i + 1];
@@ -85,9 +87,9 @@ int	append_outfile(char **args, int i, t_cmd_info *cmd)
 			return (set_err(cmd, ERR_NO_FILE, file_name));
 	}
     // case >>outfile
-	else if (ft_strlen(args[i]) > 2)
+	else if (ft_strlen(addr) > 2)
 	{
-		cmd->outfile = open(args[i] + 2, O_WRONLY | O_APPEND | O_CREAT, 0644);
+		cmd->outfile = open(addr + 2, O_WRONLY | O_APPEND | O_CREAT, 0644);
 		if (cmd->outfile < 0)
 			return (304);
 	}
--- src/parser/parser.h
+++ src/parser/parser.h
@@ -6,7 +6,7 @@
 /*   By: jhelbig <jhelbig@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/18 09:49:59 by jhelbig           #+#    #+#             */
-/*   Updated: 2025/04/20 16:51:54 by uschmidt         ###   ########.fr       */
+/*   Updated: 2025/05/05 15:06:13 by jhelbig          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,12 +42,8 @@ int			create_mask(t_parser_info *p_info, t_list **malloc_list, t_cmd_info *cmd);
 
 //outfile.c
 int			set_outfile(char **args, t_cmd_info *cmd);
-int			found_outfile(char **args, int i, t_cmd_info *cmd);
+int			found_outfile(char **args, int i, char *addr, t_cmd_info *cmd);
 int			truncate_outfile(char *file_name, t_cmd_info *cmd);
-int			append_outfile(char **args, int i, t_cmd_info *cmd);
+int			append_outfile(char **args, int i, char *addr, t_cmd_info *cmd);
 
-//parser_error.c
-//void	free_args_split(char **args);
-void		no_infile(char *file_name, t_list **malloc_list);
-void		parse_error_near_nl(t_list **malloc_list);
 #endif
