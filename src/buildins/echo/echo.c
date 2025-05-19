22196c1eef9945891c0644fd6b9a0a0c254ae36d
Urs Schmidt <ufogreeks@gmail.com>
2025-05-19 15:19:21 +0200

    fix: echo multi flags

--- src/buildins/echo/echo.c
+++ src/buildins/echo/echo.c
@@ -6,7 +6,7 @@
 /*   By: jhelbig <jhelbig@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/10 11:53:37 by uschmidt          #+#    #+#             */
-/*   Updated: 2025/05/14 14:47:04 by jhelbig          ###   ########.fr       */
+/*   Updated: 2025/05/19 15:18:16 by uschmidt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,28 @@
 int	echo(t_app *app, t_cmd_info *cmd)
 {
 	int		i;
+	int		j;
 	int		new_line;
 	char	**args;
 
 	new_line = 1;
 	i = 0;
+	j = 1;
 	if (!app)
 		return (0);
 	args = cmd->args + 1;
-	if (*args && !ft_strcmp(*args, "-n"))
+	while (*args && (*args)[0] == '-')
 	{
-		args++;
-		new_line = 0;
+		while ((*args)[j] == 'n')
+			j++;
+		if ((*args)[j] == '\0')
+		{
+			new_line = 0;
+			args++;
+			j = 1;
+		}
+		else
+			break ;
 	}
 	while (args[i])
 	{
