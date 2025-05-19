5307581a705dedc372b814996a9c87329c4dda1d
Urs Schmidt <ufogreeks@gmail.com>
2025-05-19 16:32:39 +0200

    chores: echo norm

--- src/buildins/echo/echo.c
+++ src/buildins/echo/echo.c
@@ -6,38 +6,45 @@
 /*   By: jhelbig <jhelbig@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/10 11:53:37 by uschmidt          #+#    #+#             */
-/*   Updated: 2025/05/19 15:18:16 by uschmidt         ###   ########.fr       */
+/*   Updated: 2025/05/19 16:31:41 by uschmidt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "echo.h"
 
-int	echo(t_app *app, t_cmd_info *cmd)
+char	**jump_flags(char **args, int *new_line)
 {
-	int		i;
 	int		j;
-	int		new_line;
-	char	**args;
 
-	new_line = 1;
-	i = 0;
 	j = 1;
-	if (!app)
-		return (0);
-	args = cmd->args + 1;
 	while (*args && (*args)[0] == '-')
 	{
 		while ((*args)[j] == 'n')
 			j++;
 		if ((*args)[j] == '\0')
 		{
-			new_line = 0;
+			*new_line = 0;
 			args++;
 			j = 1;
 		}
 		else
 			break ;
 	}
+	return (args);
+}
+
+int	echo(t_app *app, t_cmd_info *cmd)
+{
+	int		i;
+	int		new_line;
+	char	**args;
+
+	new_line = 1;
+	i = 0;
+	if (!app)
+		return (0);
+	args = cmd->args + 1;
+	args = jump_flags(args, &new_line);
 	while (args[i])
 	{
 		if (args[i + 1])
