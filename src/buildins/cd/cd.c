73a1a9b18dfd26d30ac2f9025c117f85110fe9a2
Urs Schmidt <ufogreeks@gmail.com>
2025-06-11 09:13:25 +0200

    fix: cd not always working

--- src/buildins/cd/cd.c
+++ src/buildins/cd/cd.c
@@ -48,25 +48,6 @@ static char	*add_to_home_route(t_app *app, char *path)
 	return (add_to_malloc_list(&app->malloc_list, abs_path), abs_path);
 }
 
-static char	*add_to_path(t_app *app, char *cur_path, char *new_path)
-{
-	char	*abs_path;
-	int		len_cur_path;
-
-	len_cur_path = ft_strlen(cur_path);
-	if (!ft_strncmp("./", new_path, 2))
-		return (ft_strjoin(cur_path, new_path + 1));
-	abs_path = (char *)malloc_and_add_list(&app->malloc_list, sizeof(char)
-			* (len_cur_path + ft_strlen(new_path) + 2));
-	if (!abs_path)
-		return (NULL);
-	ft_strlcpy(abs_path, cur_path, len_cur_path + 1);
-	ft_strlcat(abs_path + len_cur_path, "/", ft_strlen(abs_path) + 2);
-	ft_strlcat(abs_path + len_cur_path + 1, new_path,
-		len_cur_path + ft_strlen(new_path) + 3);
-	return (abs_path);
-}
-
 static char	*get_abs_path(t_app *app, char *path)
 {
 	char	*cur_path;
@@ -77,30 +58,13 @@ static char	*get_abs_path(t_app *app, char *path)
 		return (path);
 	cur_path = get_env_val(app, "PWD");
 	add_to_malloc_list(&app->malloc_list, cur_path);
-	while (!ft_strncmp("../", path, 3))
-	{
-		path += 3;
-		if (remove_last_route(&cur_path))
-			return (NULL);
-	}
 	if (!ft_strncmp("~/", path, 2))
 		return (add_to_home_route(app, path));
 	if (!ft_strncmp("-\0", path, 2))
 		return (get_env_val(app, "OLDPWD"));
-	if (!ft_strncmp("..", path, 2) && path[2] == '\0')
-	{
-		if (remove_last_route(&cur_path))
-			return (NULL);
-		return (cur_path);
-	}
-	return (add_to_path(app, cur_path, path));
+	return (path);
 }
 
-// set OLDPWD as current PWD
-// get 'directory value'
-// expand args / clean string
-// set PWD
-// updateprompt if necessary
 int	cd(t_app *app, t_cmd_info *cmd)
 {
 	char	*abs_path;
